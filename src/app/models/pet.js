dd252a33b31dbf70e5351f0abe8fe172eddf82e0
Jackie Watanabe <sushiigirl@gmail.com>
2017-06-12 13:10:14 -0700

    added defaults to pet

--- src/app/models/pet.js
+++ src/app/models/pet.js
@@ -2,6 +2,13 @@ import Backbone from 'backbone';
 
 var Pet = Backbone.Model.extend({
 
+  defaults: {
+    name: 'DEFAULT',
+    age: 0,
+    breed: '',
+    vaccinated: false
+  },
+
 });
 
 export default Pet;
